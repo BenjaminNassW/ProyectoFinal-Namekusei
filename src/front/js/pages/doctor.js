79dfe764111b417ee276ebd1a0a52b797e84b578
Kdvergarab <kdvergarab@gmail.com>
2022-05-05 23:21:37 +0000

    kim avances

--- src/front/js/pages/doctor.js
+++ src/front/js/pages/doctor.js
@@ -10,6 +10,16 @@ export const Doctor = () => {
         <input placeholder="Comuna" className="me-1"></input>
         <button className="btn-primary">Buscar</button>
       </div>
+      <div className="profile">
+        <div className="image"></div>
+        <div className="description d-flex">
+          <img
+            className="img"
+            src="https://i.pinimg.com/474x/9a/c3/73/9ac3737b869483f4e682712dc5d3c313--hospitals-doctors.jpg"
+          ></img>
+          <h1>Maria</h1>
+        </div>
+      </div>
     </div>
   );
 };
